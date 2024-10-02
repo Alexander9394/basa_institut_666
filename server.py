ddc759e0b5bf4d327038d400ace235d6c5c33920
Batman <144011987+Alexander9394@users.noreply.github.com>
2024-10-02 11:52:32 +0700

    server.py

--- server.py
+++ server.py
@@ -33,7 +33,162 @@ class Groups(Resource):
         except Exception as e:
             print(str(e))
 
-api.add_resource(Groups, '/groups')
+class Faculties(Resource):
+    faculties_list = []
 
+    def get(self):
+        self.faculties_list.clear()
+        self.load_faculties()
+        return self.faculties_list
+
+    def load_faculties(self):
+        try:
+
+            conn = psycopg2.connect(host='localhost', user='test', password='123', database='institut', port='5432')
+
+            cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+            cursor.execute('SELECT * FROM faculties')
+            for row in cursor:
+                id = row['id']
+                name = row['title']
+                self.faculties_list.append({'id': id, 'name': name})
+
+            conn.close()
+        except Exception as e:
+            print(str(e))
+
+
+class Days(Resource):
+    days_list = []
+
+    def get(self):
+        self.days_list.clear()
+        self.load_days()
+        return self.days_list
+
+    def load_days(self):
+        try:
+
+            conn = psycopg2.connect(host='localhost', user='test', password='123', database='institut', port='5432')
+
+            cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+            cursor.execute('SELECT * FROM days')
+            for row in cursor:
+                id = row['id']
+                name = row['name']
+                self.days_list.append({'id': id, 'name': name})
+
+            conn.close()
+        except Exception as e:
+            print(str(e))
+
+class Disciplines(Resource):
+    disciplines_list = []
+
+    def get(self):
+        self.disciplines_list.clear()
+        self.load_disciplines()
+        return self.disciplines_list
+
+    def load_disciplines(self):
+        try:
+
+            conn = psycopg2.connect(host='localhost', user='test', password='123', database='institut', port='5432')
+
+            cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+            cursor.execute('SELECT * FROM disciplines')
+            for row in cursor:
+                id = row['id']
+                name = row['name']
+                self.disciplines_list.append({'id': id, 'name': name})
+
+            conn.close()
+        except Exception as e:
+            print(str(e))
+
+class Teachers(Resource):
+    teachers_list = []
+
+    def get(self):
+        self.teachers_list.clear()
+        self.load_teachers()
+        return self.teachers_list
+
+    def load_teachers(self):
+        try:
+
+            conn = psycopg2.connect(host='localhost', user='test', password='123', database='institut', port='5432')
+
+            cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+            cursor.execute('SELECT * FROM teachers')
+            for row in cursor:
+                id = row['id']
+                name = row['fio']
+                self.teachers_list.append({'id': id, 'name': name})
+
+            conn.close()
+        except Exception as e:
+            print(str(e))
+
+class Rooms(Resource):
+    rooms_list = []
+
+    def get(self):
+        self.rooms_list.clear()
+        self.load_rooms()
+        return self.rooms_list
+
+    def load_rooms(self):
+        try:
+
+            conn = psycopg2.connect(host='localhost', user='test', password='123', database='institut', port='5432')
+
+            cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+            cursor.execute('SELECT * FROM rooms')
+            for row in cursor:
+                id = row['id']
+                name = row['num']
+                id_corpus = row ['id_corpus']
+                self.rooms_list.append({'id': id, 'name': name, 'id_corpus': id_corpus})
+
+            conn.close()
+        except Exception as e:
+            print(str(e))
+class Times(Resource):
+    times_list = []
+
+    def get(self):
+        self.times_list.clear()
+        self.load_times()
+        return self.times_list
+
+    def load_times(self):
+        try:
+
+            conn = psycopg2.connect(host='localhost', user='test', password='123', database='institut', port='5432')
+
+            cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+            cursor.execute('SELECT * FROM times')
+            for row in cursor:
+                id = row['id']
+                name = row['time']
+                self.times_list.append({'id': id, 'name': name})
+
+            conn.close()
+        except Exception as e:
+            print(str(e))
+api.add_resource(Groups, '/groups')
+api.add_resource(Faculties, '/faculties')
+api.add_resource(Days, '/days')
+api.add_resource(Disciplines, '/disciplines')
+api.add_resource(Teachers, '/teachers')
+api.add_resource(Rooms, '/rooms')
+api.add_resource(Times, '/times')
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5001)
